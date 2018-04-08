1a2d072e0b890c22ce9b51348f2e1deefb05cbe6
unknown <ktpm489@gmail.com>
2018-04-08 14:56:45 +0700

    Add Big Annotation

---
+++ src/Array.java
@@ -0,0 +1,19 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int[] intArray = new int[7];
+		intArray[0] = 20;
+		intArray[1] = 34;
+		intArray[2] = -23;
+		intArray[3] = 40;
+		intArray[4] = 50;
+		intArray[5] = -29;
+		intArray[6] = -90;
+		for (int i : intArray) {
+			System.out.println(i);
+		}
+	}
+
+}
